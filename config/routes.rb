50948f8fe71ac754644748928a2cb4843683aea1
Sameer Subba <sameer.libang@gmail.com>
2015-05-27 14:32:30 +0545

    category show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'home', to: 'videos#index'
   get 'videos/:id', to: 'videos#show', as: :video
+  get 'categories/:id', to: 'categories#show', as: :category
 end
