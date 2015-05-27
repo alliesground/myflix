5c6fc3417524d00da2f4a194e4d65e272a875c29
Sameer Subba <sameer.libang@gmail.com>
2015-05-27 11:57:32 +0545

    add categories

---
+++ db/migrate/20150527052856_add_ref_to_videos.rb
@@ -0,0 +1,5 @@
+class AddRefToVideos < ActiveRecord::Migration
+  def change
+    add_reference :videos, :category, index: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,43 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Video.destroy_all
+Category.destroy_all
 
-Video.create([
+tv_commedy = Category.create!(name: 'tv commedies')
+tv_drama = Category.create!(name: 'tv dramas')
+tv_reality = Category.create!(name: 'reality tv')
+
+Video.create!([
 	{
 		title: 'monk',
 		description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
 		small_cover_url: '/tmp/monk.jpg',
-		large_cover_url: '/tmp/monk_large.jpg'
+		large_cover_url: '/tmp/monk_large.jpg',
+		category: tv_commedy
 	},
 	{
 		title: 'futurama',
 		description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
 		small_cover_url: '/tmp/futurama.jpg',
-		large_cover_url: '/tmp/monk_large.jpg'
-	}
+		large_cover_url: '/tmp/monk_large.jpg',
+		category: tv_commedy
+	},
+	{
+		title: 'monk',
+		description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+		small_cover_url: '/tmp/monk.jpg',
+		large_cover_url: '/tmp/monk_large.jpg',
+		category: tv_drama
+	},
+	{
+		title: 'futurama',
+		description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+		small_cover_url: '/tmp/futurama.jpg',
+		large_cover_url: '/tmp/monk_large.jpg',
+		category: tv_reality
+	},
 ])
 
-puts "#{Video.count} records created in video model"
+puts "#{Video.count} videos created"
+puts "#{Category.count} categories created"
+
