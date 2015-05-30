d3b316d4ec8b1625e07e4497397eccd507332fd3
Sameer Subba <sameer.libang@gmail.com>
2015-05-30 15:37:25 +0545

    test and implement #recent_videos for Category model

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :category do
-  	name 'tv commedy'
+  	name 'commedy'
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -15,4 +15,35 @@ describe Category do
 
 		expect(cat_commedy.videos).to eq([futurama, south_park])
 	end
+
+	describe '#recent_videos' do
+		before :each do
+			@cat_drama = create(:category, name: 'drama')
+			@cat_reality = create(:category, name: 'reality')
+			@videos = create_list(:video, 7, category: @cat_reality)
+		end
+
+		it "returns latest videos" do
+			expect(@cat_reality.recent_videos.first).to eq @videos.last
+		end
+
+		context "with less than 6 videos" do
+			it "returns all of it" do
+				create(:video, category: @cat_drama)
+				expect(@cat_drama.recent_videos.size).to eq(1)
+			end
+		end
+
+		context "with more than 6 videos" do
+			it "returns most recent 6" do
+				expect(@cat_reality.recent_videos.size).to eq(6)
+			end
+		end
+
+		context "with no videos in the category" do
+			it "returns empty array" do
+				expect(@cat_drama.recent_videos).to match_array([])
+			end
+		end
+	end
 end
