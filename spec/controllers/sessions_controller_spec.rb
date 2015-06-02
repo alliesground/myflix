4c546f21a91b4db73d0f3d75b67a4222ec0a1fc0
Sameer Subba <sameer.libang@gmail.com>
2015-06-02 17:52:15 +0545

    controller test for authentication

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,19 +2,20 @@ require 'spec_helper'
 
 describe SessionsController do
   describe "POST #create" do
-    context "with valid attributes" do
-      let(:user){ create(:user, email: 'user@example.com') }
-      it "logs in the user" do
-        post :create, email: user.email, password: 'wildhorses'
-        expect(session[:user_id]).to eq user.id
-      end
+    let(:user){ create(:user) }
+    it "logs in user with valid attributes" do
+      post :create, email: user.email, password: 'wildhorses'
+      expect(session[:user_id]).to eq user.id
     end
 
-    context "with invalid attributes" do
-      it "renders the :new template" do
-        post :create, session: {email: 'invalid@example.com', password:'wildroses'}
-        expect(response).to render_template :new
-      end
+    it "renders :new template for invalid email" do
+      post :create, email: 'invalid@example.com', password: 'wildhorses'
+      expect(response).to render_template :new
+    end
+
+    it "renders :new template for invalid password" do
+      post :create, email: user.email, password: 'wildroses'
+      expect(response).to render_template :new
     end
   end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe 'GET #new' do
+    it "creates an instance of User model" do
+      get :new
+      expect(assigns(:user)).to be_instance_of(User)
+    end
+  end
+
   describe 'POST #create' do
     context "with valid attributes" do
       it "saves the new user" do
