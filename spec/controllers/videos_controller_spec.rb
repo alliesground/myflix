821cb9c67e1e252f3cf41eb17ac9fa6ef7d9b566
Sameer Subba <sameer.libang@gmail.com>
2015-06-02 13:31:33 +0545

    controller test for video#show and video#search

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -5,11 +5,43 @@ describe VideosController do
     @user = create(:user)
     session[:user_id] = @user.id
   end
+
+  describe 'GET #show' do
+    it "finds the requested video" do
+      video = create(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq video
+    end
+
+    it "does not find the requested video" do
+      get :show, id: 1
+      expect(assigns(:video)).to eq nil
+    end
+  end
+
   describe 'POST #search' do
-    it "assigns the search results to @search_result" do
-      futurama = create(:video)
-      get :search, search: "futurama"
-      expect(assigns(:search_result)).to match_array([futurama])
+    context "with no match" do
+      it "assigns an empty array to @search_result" do
+        get :search, search: "futurama"
+        expect(assigns(:search_result)).to match_array([])
+      end
+    end
+
+    context "with single match" do
+      it "assigns the result to @search_result" do
+        futurama = create(:video)
+        get :search, search: "futurama"
+        expect(assigns(:search_result)).to match_array([futurama])
+      end
+    end
+
+    context "with multiple match" do
+      it "assigns multiple results to @search_result" do
+        south_country = create(:video, title: 'south_country')
+        south_park = create(:video, title: 'south_park')
+        get :search, search: "south"
+        expect(assigns(:search_result)).to match_array([south_country, south_park])
+      end
     end
   end
 end
