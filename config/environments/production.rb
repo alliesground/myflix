db6f1f7ab499245e1584b32c1936055193adc55b
Sameer Subba <sameer.libang@gmail.com>
2015-06-18 17:57:03 +0545

    set action mailer delivery error true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,6 @@ Myflix::Application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.default_url_options = { :host => 'https://lit-earth-1464.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
