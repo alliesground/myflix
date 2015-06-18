2acdfe1204d7f12a9b730e483e1e2fb7a3087295
Sameer Subba <sameer.libang@gmail.com>
2015-06-18 17:43:57 +0545

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,11 @@ Myflix::Application.configure do
   config.action_mailer.default_url_options = { :host => 'https://lit-earth-1464.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com'
+    domain:               'example.com',
     user_name:            ENV['gmail_username'],
     password:             ENV['gmail_password'],
     authentication:       'plain',
