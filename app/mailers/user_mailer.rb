9f8fd6369373ffedfdc1c7a61d152e5e5c86ff5a
Sameer Subba <sameer.libang@gmail.com>
2015-06-19 22:16:44 +0545

    mailer changes for staging

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   def welcome_registered_user(user)
     @user = user
-    mail from: "admin@myflix.com", to: @user.email, subject: "Welcome to Myflix"
+    mail from: "admin@myflix.com", to: ENV['admin_email'] || @user.email, subject: "Welcome to Myflix"
   end
 
   def send_password_reset_link(user)
